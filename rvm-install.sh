d909b2f3f70266afd8d465438bd3929338b5a74e
Jeroen de Vries <jeroen@trackconcepts.com>
2016-02-03 16:40:49 +0100

    Removed commented code.

--- rvm-install.sh
+++ rvm-install.sh
@@ -5,9 +5,3 @@
 echo 'Installing rvm ruby manager'
 
 curl -sSL https://get.rvm.io | bash
-
-#source /home/vagrant/.profile
-
-# # Install Ruby Enterprise 1.8.7, this can take a while
-# rvm install ree-1.8.7
-# rvm use ree-1.8.7-2012.02 --default --create
